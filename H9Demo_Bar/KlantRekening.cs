9816f2f5c7cfb58e1f701dbb97b1041df728bc48
Tim Dams <dams.tim@telenet.Be>
2022-05-15 21:40:04 +0200

    Add project files.

---
+++ H9Demo_Bar/KlantRekening.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H9Demo_Bar
+{
+    class KlantRekening
+    {
+        private List<int> kosten = new List<int>();
+
+        public void VoegToe(int aantal, int prijs)
+        {
+            kosten.Add(aantal * prijs);
+        }
+
+        public void Rekenaf()
+        {
+            int som = BerekenSom();
+            Console.WriteLine($"Totaal = {som}");
+            kosten.Clear();
+
+        }
+
+        private int BerekenSom()
+        {
+            int som = 0;
+            foreach (var kost in kosten)
+            {
+                som += kost;
+            }
+
+            return som;
+        }
+
+        public override string ToString()
+        {
+            return BerekenSom().ToString();
+        }
+    }
+}
---
+++ H9Demo_Bar/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace H9Demo_Bar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool happyHour = false;
+            List<KlantRekening> klanten = new List<KlantRekening>();
+            for (int i = 0; i < 10; i++)
+            {
+                klanten.Add(new KlantRekening());
+            }
+
+            string keuze = "";
+            do
+            {
+                Console.Clear();
+                ToonTitel();
+                PrintAlleRekeningen(klanten);
+                keuze = ToonMenuEnVraagInvoer();
+                switch (keuze)
+                {
+                    case "a":
+                        Console.WriteLine("Welke klantnummer? (0-9)");
+                        int klant = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Hoeveel consumpties?");
+                        int aantal = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Prijs consumpties?");
+                        int prijs = int.Parse(Console.ReadLine());
+                        klanten[klant].VoegToe(aantal, prijs);
+                        break;
+                    case "r":
+                        Console.WriteLine("Welke klantnummer? (0-9)");
+                        int klantr = int.Parse(Console.ReadLine());
+                        klanten[klantr].Rekenaf();
+                        break;
+
+                    case "h":
+                        happyHour = !happyHour;
+                        if(!happyHour)
+                            Console.WriteLine("Happy hour niet actief");
+                        else Console.WriteLine("Happy hour actief");
+                      
+                        break;
+                    default:
+                        break;
+                }
+                Console.ReadKey();
+            } while (keuze!="q");
+        }
+
+        private static void PrintAlleRekeningen(List<KlantRekening> klanten)
+        {
+            for (int i = 0; i < klanten.Count; i++)
+            {
+                Console.WriteLine($"{i}: {klanten[i]}");
+            }
+        }
+
+        private static string ToonMenuEnVraagInvoer()
+        {
+            Console.WriteLine("Wat wil je doen?");
+            Console.WriteLine("\t a : order invoeren");
+            Console.WriteLine("\t r : afrekenen ");
+            Console.WriteLine("\t h : happy hour starten/stoppen");
+            Console.WriteLine("\t q : stoppen");
+            return Console.ReadLine();
+        }
+
+        private static void ToonTitel()
+        {
+            string title = @"
+__________________ _______  _______    ______   _______  _______ 
+\__   __/\__   __/(       )(  ____ \  (  ___ \ (  ___  )(  ____ )
+   ) (      ) (   | () () || (    \/  | (   ) )| (   ) || (    )|
+   | |      | |   | || || || (_____   | (__/ / | (___) || (____)|
+   | |      | |   | |(_)| |(_____  )  |  __ (  |  ___  ||     __)
+   | |      | |   | |   | |      ) |  | (  \ \ | (   ) || (\ (   
+   | |   ___) (___| )   ( |/\____) |  | )___) )| )   ( || ) \ \__
+   )_(   \_______/|/     \|\_______)  |/ \___/ |/     \||/   \__/
+                                                                 
+";
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine(title);
+            Console.ResetColor();
+        }
+    }
+}
